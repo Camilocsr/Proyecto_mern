4d7bc8805beb174aafdd8a60fb0663c5d7afb59f
Camilocsr <esmunred@gmail.com>
2024-07-06 16:26:55 -0500

    se restructura arpetas de aws y auth de as

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const upload = require('../libs/storage');
-const { addProduct,getProducts,deleteProducts,editionProducts } = require('../controllers/productController')
+const { addProduct } = require('../controllers/products/addProducts.js')
+const { getProducts } = require('../controllers/products/getProducts.js')
+const { deleteProducts } = require('../controllers/products/deleteProducts.js')
+const { editionProducts } = require('../controllers/products/editProducts.js')
 const api = express.Router();
 
 api.post('/products',upload.single('image'), addProduct);
